ff319fcd330dce83c0fe769168d922297d79aa04
Dani Lukman Hakim <dani.lukman@ad.balitower.co.id>
2025-01-30 16:52:20 +0700

    asd

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
